a9e181e572e0c3320f473db7ed1ad0a388b1a2dc
Yash Choudhary <yashchoudhary13@outlook.com>
2026-01-23 11:10:46 +0530

    Add Shield icon as favicon

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import React from "react";
 import { appWithTranslation } from "next-i18next";
+import Head from "next/head";
 
 // Query caching + UI providers
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -14,16 +15,23 @@ import PageTransition from "@/components/PageTransition";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <TooltipProvider>
-          <Toaster />
-          <PageTransition>
-            <Component {...pageProps} />
-          </PageTransition>
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon-colored.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#6366f1" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <TooltipProvider>
+            <Toaster />
+            <PageTransition>
+              <Component {...pageProps} />
+            </PageTransition>
+          </TooltipProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </>
   );
 }
 
